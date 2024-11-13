c004301339124b80c7c068b8dc9c877cf5de60d7
carson <rc.carsongao@ricacorp.com>
2024-11-13 14:31:58 +0800

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3733076187"
+    serviceWorkerVersion: "3818883223"
   }
 });
